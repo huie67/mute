ace2a4fd20b669a2a8e1bbeed55d3ff46d1b0820
huie67 <dimon333fox@gmail.com>
2026-09-23 18:02:04 +0300

    Delete desktop directory

--- desktop/preload-picker.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('pickerAPI', {
-    onSources: (callback) => ipcRenderer.on('sources', (event, sources) => callback(sources)),
-    select: (id) => ipcRenderer.send('source-selected', id),
-    cancel: () => ipcRenderer.send('source-selected', null)
-});
